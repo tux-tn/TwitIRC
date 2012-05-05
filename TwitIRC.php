88101fe2a382a537b41a9da0ae2aaa10fde3d1c1
Sarhan Aissi <sarhanaissi@gmail.com>
2012-05-05 20:37:38 +0200

    Adding an url unshortener

--- TwitIRC.php
+++ TwitIRC.php
@@ -11,6 +11,7 @@ $server = "irc.freenode.net"; // IRC server
 $port = "6667"; // The port of the IRC server
 $account = "support"; // Twitter username
 $minute = 15; // Interval in minutes of checking for new tweets
+$pattern = '$\b(https?|ftp|file)://[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|]$i'; // Awesome brain fuck :D
 $socket = fsockopen($server,$port);
 if(!$socket)
 {
@@ -22,7 +23,17 @@ fputs($socket, "NICK $pseudo\r\n");
 fputs($socket, "JOIN $channel\r\n");
 $RSS =0;
 $last = "";
-
+function unshortener($url){
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$yy = curl_exec($ch);
+curl_close($ch);
+$w = explode("\n",$yy);
+$real_url = substr($w[3],10);
+return $real_url;
+} // Function to get original URL from a shortened one
 while(1)
 {
 $donnees =  fgets($socket, 1024);
@@ -39,7 +50,15 @@ if($donnees){
 		curl_setopt($c, CURLOPT_HEADER, true);
 		$output = curl_exec($c);
 		if(preg_match_all("#<description>(.+)</description>#S", $output, $last_tweet) && $last != $last_tweet[1][1]){
-			fputs($socket, "PRIVMSG $channel :New Tweet: @" . html_entity_decode($last_tweet[1][1], ENT_QUOTES, "UTF-8") . "\r\n");
+			$string = html_entity_decode($last_tweet[1][1], ENT_QUOTES, "UTF-8");
+			preg_match_all($pattern, $string, $result, PREG_PATTERN_ORDER); // putting all shortened URLs in the tweet into an array
+			$A = $result[0];
+			$count = count($A);
+			for ($i = 0; $i < $count; $i++) {
+				$A[$i] = unshortener($A[$i]);
+				}
+			$R = str_replace($result[0],$A,$string);
+			fputs($socket, "PRIVMSG $channel :[Twitter] $R \r\n");
 			$last = $last_tweet[1][1];
 			}
 		$RSS= 0;
